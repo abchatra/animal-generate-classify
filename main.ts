698fc9327e7759e85c384e576fbfcdf26c9f9bf5
Abhijith Chatra <abchatra@microsoft.com>
2021-10-14 12:48:39 -0700

    First round of blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,40 @@
- 
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+
+namespace animal {
+
+    export enum type {
+        //% block="chicken"
+        chicken,
+        //% block="crow"
+        crow,
+        //% block="lion"
+        lion
+    }
+
+    //% block
+    let updateHandler: (animal: number) => void;
+
+    //% block
+    export function generate(): void {
+        
+    }
+    //% block
+    export function onClassifyUpdate(handler: (animal: number) => void) {
+        updateHandler = handler;
+    }
+
+    //% block
+    export function classify(value: number): number {
+        init();
+        updateHandler(value);
+        return value;
+   }
+
+    function init() {
+
+    }
+
+}
